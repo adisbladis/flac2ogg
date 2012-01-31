cfd123cdeb83489fcb220202a4861a01be650cb0
adisbladis <adis@blad.is>
2012-01-31 09:48:41 +0100

    Fixed quoting

--- flac2ogg.py
+++ flac2ogg.py
@@ -16,16 +16,19 @@ import sys
 import os
 import shutil
 import os.path
+from pipes import quote
 
 parser = argparse.ArgumentParser(description='Convert a directory tree from flac to ogg and copy the rest')
 parser.add_argument('input', action='store', type=str)
 parser.add_argument('output', action='store', type=str)
 parser.add_argument('-t', action='store', type=int, default=multiprocessing.cpu_count(), help='Number of concurrent processes (default <number of cores>)')
 parser.add_argument('-q', action='store', type=str, default=8, help='Oggenc quality (default 8)')
-parser.add_argument('-o', action='store', type=str, default="", help='Oggenc extra options')
+parser.add_argument('-o', action='store', type=str, default='', help='Oggenc extra options')
 args=parser.parse_args()
 
 def run_command(commandline):
+
+    #Fix commandline
     p=subprocess.Popen(shlex.split(commandline),stderr=subprocess.PIPE,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
     if p.wait() != 0:
         sys.stderr.write('ERROR IN: %s\n%s\n\n' % (commandline,p.communicate()[1]))
@@ -33,10 +36,10 @@ def run_command(commandline):
 if __name__ == '__main__':
 
     #Sanitize input
-    if args.input[-1] != "/":
-        args.input+="/"
-    if args.output[-1] != "/":
-        args.output+="/"
+    if args.input[-1] != '/':
+        args.input+='/'
+    if args.output[-1] != '/':
+        args.output+='/'
 
     #Does the new dir exist
     if(not os.path.isdir(args.output)):
@@ -51,7 +54,7 @@ if __name__ == '__main__':
     commandlines=list()
     for root, dirs, files in os.walk(args.input):
         for name in dirs:
-            print("Creating %s" % (name))
+            print('Creating %s' % (name))
             try: os.mkdir(os.path.join(root, name).replace(args.input,args.output,1))
             except OSError: pass #Already exists                
 
@@ -61,22 +64,22 @@ if __name__ == '__main__':
             input_file=os.path.join(root, name)
             output_file=os.path.join(root, name).replace(args.input,args.output,1)
 
-            if name.split(".")[-1].lower() == "flac":
-                output_file=output_file.rpartition(".")[0]+".ogg"
-                commandline="oggenc "
-                commandline+="-Q -q %s " % (args.q)
+            if name.split('.')[-1].lower() == 'flac':
+                output_file=output_file.rpartition('.')[0]+'.ogg'
+                commandline='oggenc '
+                commandline+='-Q -q %s ' % (args.q)
                 commandline+=args.o
-                commandline+=" -o "+output_file.replace(" ","\ ")+" "+input_file.replace(" ","\ ")
+                commandline+=' -o '+quote(output_file)+' '+quote(input_file)
                 commandlines.append(commandline)
             else:
-                print("Copying %s" % (name))
+                print('Copying %s' % (name))
                 shutil.copy(input_file,output_file)
 
     #Clear queue
     while len(commandlines) > 0:
         for i in range(0,len(process_pool)):
             if process_pool[i].is_alive() == False:
-                print(str(len(commandlines)) + " files left to encode")
+                print(str(len(commandlines)) + ' files left to encode')
                 process_pool[i] = Process(target=run_command, args=(commandlines.pop(),))
                 process_pool[i].start()
         time.sleep(0.1)
